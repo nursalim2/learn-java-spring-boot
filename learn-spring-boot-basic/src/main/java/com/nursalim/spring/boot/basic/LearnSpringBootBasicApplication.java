3b0b81bb03b6f6e0984a7f399266b6fccc9d5b8e
Nursalim <nursalim.me@gmail.com>
2025-04-09 17:06:23 +0700

    learn spring boot core

---
+++ learn-spring-boot-basic/src/main/java/com/nursalim/spring/boot/basic/LearnSpringBootBasicApplication.java
@@ -0,0 +1,13 @@
+package com.nursalim.spring.boot.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootBasicApplication.class, args);
+	}
+
+}
---
+++ learn-spring-boot-basic/src/test/java/com/nursalim/spring/boot/basic/LearnSpringBootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nursalim.spring.boot.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
