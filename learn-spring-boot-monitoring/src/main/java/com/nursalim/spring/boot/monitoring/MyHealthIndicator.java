7829e7c2554438a1e55911ac7d331e7f1b4ebcc6
Nursalim <nursalim.me@gmail.com>
2025-04-24 15:57:21 +0700

    learn spring boot monitoring

---
+++ learn-spring-boot-monitoring/src/main/java/com/nursalim/spring/boot/monitoring/MyHealthIndicator.java
@@ -0,0 +1,16 @@
+package com.nursalim.spring.boot.monitoring;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyHealthIndicator extends AbstractHealthIndicator {
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        builder.status(Status.DOWN);
+        builder.withDetail("app", "OK");
+        builder.withDetail("error", "NO ERROR");
+    }
+}
---
+++ learn-spring-boot-monitoring/src/test/java/com/nursalim/spring/boot/monitoring/LearnSpringBootMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nursalim.spring.boot.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
